837a20be4992b7e0aa66dd67e97cb4f0c7e08dfb
artur artur <arturk102007@gmail.com>
2024-09-06 14:26:15 +0300

    Убрал функцию Sum

--- 05.09.24/05.09.24.cpp
+++ 05.09.24/05.09.24.cpp
@@ -1,14 +1,11 @@
 #include <iostream>
 using namespace std;
 
-int Sum(int a, int b)
-{
-    return a + b;
-}
-
 int main()
 {
     
-    std::cout << "Hello World!\n";
+    cout << "Hello World!\n";
     cout << "Hello world" << endl;
+
+
 }
